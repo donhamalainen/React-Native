4b58a5f1cc39c7c5a89953adbdcc029ca6d57850
Don <hamalainen.don@gmail.com>
2023-05-21 01:40:34 +0300

    GameScreen

--- pokerNoter/screens/GameScreen.js
+++ pokerNoter/screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Keyboard,
   KeyboardAvoidingView,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 
@@ -13,11 +14,15 @@ import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 // Firebase
 import { database, auth } from "../config/firebaseConfig";
-import { get, ref, remove, set, onValue } from "firebase/database";
+import { get, ref, remove, set, onValue, off } from "firebase/database";
 
 const GameScreen = ({ GameOnline }) => {
+  // Variables
   const [inGame, setInGame] = useState(true);
   const [endGame, setEndGame] = useState(false);
+  // Peli-istunnon tiedot
+  const [gameSessionData, setGameSessionData] = useState(null);
+
   // Siirry odotustilaan
   const goWaitingMode = async () => {
     // Haetaan sessionId
@@ -59,7 +64,7 @@ const GameScreen = ({ GameOnline }) => {
 
     onValue(pelaajatRef, (snap) => {
       const pelaajat = snap.val();
-      console.log(pelaajat);
+      //console.log(pelaajat);
       if (!pelaajat || Object.keys(pelaajat).length === 0) {
         setEndGame(true);
       }
@@ -68,21 +73,206 @@ const GameScreen = ({ GameOnline }) => {
 
   useEffect(() => {
     checkEnd();
-  }, [goWaitingMode]);
+  }, [inGame]);
+
+  // Päivitä peli-istunnon tiedot
+  const updateGameSessionData = async () => {
+    const sessionId = await AsyncStorage.getItem("@game");
+    const gameSessionRef = ref(database, `pelit/${sessionId}`);
+    const listener = onValue(gameSessionRef, (snapshot) => {
+      setGameSessionData(snapshot.val());
+    });
+    return () => {
+      off(gameSessionRef, listener);
+    };
+  };
+
+  // Hae tiedot, kun komponentti ladataan ja päivitä niitä muutosten yhteydessä
+  useEffect(() => {
+    updateGameSessionData();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <View style={{ flex: 1, paddingHorizontal: 25 }}>
-        <Text>GameScreen</Text>
-        <TouchableOpacity onPress={() => goWaitingMode()}>
-          <Text>Siirry odotustilaan</Text>
-        </TouchableOpacity>
+        {/* Header */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {/* Title */}
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>PokerNoter</Text>
+          {/* TIMER 
+           <Text style={{ fontSize: 14, fontWeight: "bold" }}>hh:mm:ss</Text>
+          */}
+        </View>
+        {/* Pöytä tiedot */}
+        <View
+          style={{
+            backgroundColor: "#212A3E",
+            padding: 20,
+            gap: 20,
+            borderRadius: 10,
+            marginTop: 10,
+          }}
+        >
+          {/* Pelaajia pöydässä */}
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontStyle: "italic",
+                fontWeight: "600",
+                color: "white",
+              }}
+            >
+              Pelaajia pöydässä:
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                fontStyle: "italic",
+                fontWeight: "400",
+                color: "white",
+              }}
+            >
+              {gameSessionData
+                ? Object.keys(gameSessionData.pelaajat).length
+                : 0}{" "}
+              henkilöä
+            </Text>
+          </View>
+
+          {/* Rahamäärä pöydässä */}
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                fontStyle: "italic",
+                fontWeight: "600",
+                color: "white",
+              }}
+            >
+              Rahaa pöydässä yhteensä:
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                fontStyle: "italic",
+                fontWeight: "400",
+                color: "white",
+              }}
+            >
+              {gameSessionData ? gameSessionData.kokonaisRahamaara : 0} €
+            </Text>
+          </View>
+        </View>
+
+        {/* Poistu pelistä painike */}
         {endGame ? (
           <TouchableOpacity onPress={() => leaveGame()}>
-            <Text>Poistu pelistä</Text>
+            <Text style={{ fontSize: 18 }}>Poistu pelistä</Text>
           </TouchableOpacity>
         ) : (
-          <></>
+          <TouchableOpacity onPress={() => goWaitingMode()}>
+            <Text>Siirry odotustilaan</Text>
+          </TouchableOpacity>
         )}
+
+        {/* Pelaaja lista */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 40,
+            paddingBottom: 10,
+            alignItems: "center",
+            borderBottomWidth: 2,
+            borderColor: "gray",
+          }}
+        >
+          <Text style={{ fontSize: 20 }}>Pelaajat</Text>
+          <Text>Buy In</Text>
+        </View>
+        <ScrollView>
+          {/* Aktiiviset pelaajat */}
+          {gameSessionData &&
+            gameSessionData.pelaajat &&
+            Object.entries(gameSessionData.pelaajat).map(([key, pelaaja]) => (
+              <View
+                key={key}
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  paddingVertical: 10,
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontStyle: "italic",
+                    fontWeight: "600",
+                    color: "black",
+                  }}
+                >
+                  {pelaaja.nimi}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontStyle: "italic",
+                    fontWeight: "400",
+                    color: "black",
+                  }}
+                >
+                  {pelaaja.buyIn} €
+                </Text>
+              </View>
+            ))}
+          {/* Poistuneet pelaajat */}
+          {gameSessionData &&
+            gameSessionData.poistuneetPelaajat &&
+            Object.entries(gameSessionData.poistuneetPelaajat).map(
+              ([key, pelaaja]) => (
+                <View
+                  key={key}
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    paddingVertical: 10,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontStyle: "italic",
+                      fontWeight: "600",
+                      color: "red",
+                    }}
+                  >
+                    {pelaaja.nimi}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      fontStyle: "italic",
+                      fontWeight: "400",
+                      color: "red",
+                    }}
+                  >
+                    {pelaaja.buyIn} €
+                  </Text>
+                </View>
+              )
+            )}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
